d088a9e9ec37b0033716e3a89f9384d0c7b3458f
Abhijeet Mohapatra <42213151+abhijeet1410@users.noreply.github.com>
2020-11-06 01:09:58 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,117 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:http/http.dart' as http;
+import 'package:retroinfinityscroll/retroinfinityscroll.dart';
+
+void main() {
+  runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: InfiniteScrollPage(),
+        appBar: AppBar(title: Text("Retro Scroll")),
+      )));
+}
+
+class InfiniteScrollPage extends StatefulWidget {
+  @override
+  _InfiniteScrollPageState createState() => _InfiniteScrollPageState();
+}
+
+class _InfiniteScrollPageState extends State<InfiniteScrollPage> {
+  bool _hasMore;
+  bool _error;
+  bool _loading;
+  final int _limit = 10;
+  List<Photo> _photos;
+
+  @override
+  void initState() {
+    super.initState();
+    _hasMore = true;
+    _error = false;
+    _loading = true;
+    _photos = [];
+    fetchPhotos();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return RetroListView(
+      hasMore: _hasMore,
+      itemCount: _photos.length,
+      stateType: _loading
+          ? InfiniteScrollStateType.loading
+          : _error
+              ? InfiniteScrollStateType.error
+              : InfiniteScrollStateType.loaded,
+      itemBuilder: (ctx, position) {
+        if (position >= _photos.length) {
+          if (_hasMore) {
+            fetchPhotos();
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Center(
+                child: CircularProgressIndicator(),
+              ),
+            );
+          } else
+            return Container();
+        }
+        return Card(
+          child: Column(
+            children: <Widget>[
+              Image.network(
+                _photos[position].thumbnailUrl,
+                fit: BoxFit.fitWidth,
+                width: double.infinity,
+                height: 160,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16),
+                child: Text(_photos[position].title,
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  Future<void> fetchPhotos() async {
+    try {
+      final response =
+          await http.get("https://jsonplaceholder.typicode.com/photos?_page=1");
+      List<Photo> fetchedPhotos = Photo.parseList(json.decode(response.body));
+      setState(() {
+        _hasMore = fetchedPhotos.length == _limit;
+        _loading = false;
+        _photos.addAll(fetchedPhotos);
+      });
+    } catch (e) {
+      setState(() {
+        _loading = false;
+        if (_photos.isEmpty) {
+          _error = true;
+        } else {
+          print(e);
+        }
+      });
+    }
+  }
+}
+
+class Photo {
+  final String title;
+  final String thumbnailUrl;
+  Photo(this.title, this.thumbnailUrl);
+  factory Photo.fromJson(Map<String, dynamic> json) {
+    return Photo(json["title"], json["thumbnailUrl"]);
+  }
+  static List<Photo> parseList(List<dynamic> list) {
+    return list.map((i) => Photo.fromJson(i)).toList();
+  }
+}

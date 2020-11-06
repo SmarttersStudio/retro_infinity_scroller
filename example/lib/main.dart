84a590bac1f2e158f06014da394abab1daf3652d
Abhijeet Mohapatra <42213151+abhijeet1410@users.noreply.github.com>
2020-11-06 20:49:40 +0530

    Dummy Pages WIP

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,106 +1,52 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'dart:async';
-import 'package:http/http.dart' as http;
-import 'package:retro_infinity_scroll/retro_infinity_scroll.dart';
+import 'package:retroinfinityscroll_example/pages/infinity_scroll_with_android_refresh_page.dart';
+import 'package:retroinfinityscroll_example/pages/load_more_retro_infinity_scroll_page.dart';
+import 'package:retroinfinityscroll_example/pages/simple_retro_infinity_scroll_page.dart';
 
 void main() {
   runApp(MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: InfiniteScrollPage(),
-        appBar: AppBar(title: Text("Retro Scroll Page")),
+        body: ChooseOptionsPage(),
+        appBar: AppBar(title: Text("Choose to view the implementation")),
       )));
 }
 
-class InfiniteScrollPage extends StatefulWidget {
-  @override
-  _InfiniteScrollPageState createState() => _InfiniteScrollPageState();
-}
-
-class _InfiniteScrollPageState extends State<InfiniteScrollPage> {
-  bool _hasMore;
-  bool _error;
-  bool _loading;
-  final int _limit = 10;
-  List<Photo> _photos;
-
-  @override
-  void initState() {
-    super.initState();
-    _hasMore = true;
-    _error = false;
-    _loading = true;
-    _photos = [];
-    fetchPhotos();
-  }
-
+class ChooseOptionsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return RetroListView(
-      hasMore: _hasMore,
-      itemCount: _photos.length,
-      stateType: _loading
-          ? InfiniteScrollStateType.loading
-          : _error
-              ? InfiniteScrollStateType.error
-              : InfiniteScrollStateType.loaded,
-      onLoadMore: () => fetchPhotos(),
-      refreshIndicatorType: RefreshIndicatorType.ios,
-      itemBuilder: (ctx, position) {
-        return Card(
-          child: Column(
-            children: <Widget>[
-              Image.network(
-                _photos[position].thumbnailUrl,
-                fit: BoxFit.fitWidth,
-                width: double.infinity,
-                height: 160,
-              ),
-              Padding(
-                padding: const EdgeInsets.all(16),
-                child: Text(_photos[position].title,
-                    style:
-                        TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
-              ),
-            ],
-          ),
-        );
-      },
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            RaisedButton(
+              onPressed: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (ctx) => SimpleRetroInfinityPage())),
+              child:
+                  Text('Simple Implementation (Without Refresh and loadmore)'),
+            ),
+            RaisedButton(
+              onPressed: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (ctx) => LoadMoreInfinityScrollPage())),
+              child: Text('With Loadmore Implementation only'),
+            ),
+            RaisedButton(
+              onPressed: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (ctx) =>
+                          InfinityScrollWithAndroidRefreshPage())),
+              child: Text('With Android Refresh and LoadMore Implementation'),
+            ),
+          ],
+        ),
+      ),
     );
-  }
-
-  Future<void> fetchPhotos() async {
-    try {
-      final response =
-          await http.get("https://jsonplaceholder.typicode.com/photos?_page=1");
-      List<Photo> fetchedPhotos = Photo.parseList(json.decode(response.body));
-      setState(() {
-        _hasMore = fetchedPhotos.length == _limit;
-        _loading = false;
-        _photos.addAll(fetchedPhotos);
-      });
-    } catch (e) {
-      setState(() {
-        _loading = false;
-        if (_photos.isEmpty) {
-          _error = true;
-        } else {
-          print(e);
-        }
-      });
-    }
-  }
-}
-
-class Photo {
-  final String title;
-  final String thumbnailUrl;
-  Photo(this.title, this.thumbnailUrl);
-  factory Photo.fromJson(Map<String, dynamic> json) {
-    return Photo(json["title"], json["thumbnailUrl"]);
-  }
-  static List<Photo> parseList(List<dynamic> list) {
-    return list.map((i) => Photo.fromJson(i)).toList();
   }
 }
--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:retroinfinityscroll_example/main.dart';
 void main() {
   testWidgets('Verify Platform version', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(InfiniteScrollPage());
+    await tester.pumpWidget(ChooseOptionsPage());
 
     // Verify that platform version is retrieved.
     expect(
